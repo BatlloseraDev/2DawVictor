3d5ef3a037e62170f4ea1e9bd892e2598ae0e26f
Victor Albert Bat-Llosera Gallego <86764975+BatlloseraDev@users.noreply.github.com>
2025-09-10 10:07:04 +0200

    Primera Clase
    Iniciacion

---
+++ Clase1_2/Prueba.py
@@ -0,0 +1,23 @@
+from builtins import input
+import calculadora as calc
+#print("hola")
+resultado = 10
+#resultado = "hola"
+a = str(input("Dame un número\n"))
+a = a + "10"
+#concateno = a + "dd"
+print(a)
+
+
+
+def miPrimerMetodo():
+    nombre = "Victor"
+    print(nombre)
+    apellido = "Bat"
+    print(apellido)
+    calc.sumar()
+
+
+print("saludo")
+miPrimerMetodo()
+
